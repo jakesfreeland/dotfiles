5656e03b2d0d733889f278682fd47a6f38e1b4df
Jake Freeland <jfree@freebsd.org>
2022-11-21 16:13:10 -0600

    Add enable/disable functionality to nvim lsp config

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -1,19 +1,37 @@
+--[[
+	SPDX-License-Identifier: BSD-2-Clause
+	Copyright (c) 2022, Jake Freeland <jfree@freebsd.org>
+	Contents: Minimal Neovim Language Server Protocol (LSP) interface
+]]--
+
+function lsp_lang_init(lang)
+	vim.api.nvim_create_user_command("Enable" .. lang.name .. "LSP",
+	    function() lsp_lang_attach(lang) end, {})
+	vim.api.nvim_create_user_command("Disable" .. lang.name .. "LSP",
+	    function() lsp_lang_detach(lang.name) end, {})
+	if lang.auto_attach
+		then vim.api.nvim_create_autocmd("FileType", {
+			pattern = lang.filetypes,
+			callback = function() lsp_lang_attach(lang) end
+		})
+	end
+end
+
 function lsp_lang_attach(lang)
-	vim.api.nvim_create_autocmd("FileType", {
-		pattern = lang.filetypes,
-		callback = function()
-			client = vim.lsp.get_active_clients({ name = lang.name })[1]
-			if client
-			then vim.lsp.buf_attach_client(0, client.id)
-			else vim.lsp.buf_attach_client(0, vim.lsp.start_client({
-				name = lang.name,
-				cmd = lang.cmd,
-				root_dir = vim.fs.dirname(
-				    vim.fs.find(lang.root_pattern, { upward = true })[1])
-			}))
-			end
-		end
-	})
+	client = vim.lsp.get_active_clients({ name = lang.name })[1]
+	if client
+		then vim.lsp.buf_attach_client(0, client.id)
+		else vim.lsp.buf_attach_client(0, vim.lsp.start_client({
+			name = lang.name,
+			cmd = lang.cmd,
+			root_dir = vim.fs.dirname(
+			vim.fs.find(lang.root_pattern, { upward = true })[1])
+		}))
+	end
+end
+
+function lsp_lang_detach(lang_name)
+	vim.lsp.stop_client(vim.lsp.get_active_clients({ name = lang_name }))
 end
 
 function lsp_lang_keys(set_keys)
@@ -23,11 +41,28 @@ end
 --- CONFIG ---
 
 -- languages
-lsp_lang_attach({
+lsp_lang_init({
 	name = "rust",
 	cmd = { "rust-analyzer" },
 	filetypes = { "rust" },
-	root_pattern = { "Cargo.toml", "rust-project.json" }
+	root_pattern = { "Cargo.toml", "rust-project.json" },
+	auto_attach = true
+})
+
+lsp_lang_init({
+	name = "clangd",
+	cmd = { "clangd" },
+	filetypes = { "c", "cpp", "objc", "objcpp", "cuda", "proto" },
+	root_pattern = {
+		".clangd",
+		".clang-tidy",
+		".clang-format",
+		"compile_commands.json",
+		"compile_flags.txt",
+		"configure.ac",
+		".git"
+	},
+	auto_attach = false
 })
 
 -- keybinds
