55da1bbc2f14334696a09e1a6306f4115558eb45
Jake Freeland <jfree@FreeBSD.org>
2025-10-07 19:14:45 -0500

    emacs: Clean up dired setup
    Comment out unneeded PEC changes.

--- .config/emacs/dired-setup.el
+++ .config/emacs/dired-setup.el
@@ -6,37 +6,4 @@
   :ensure nil
   :config
   ;; Overwrite useless `list-directory' binding to call `dired' instead
-  (keybind "C-x C-d" dired)
-
-  (defun dired-sysopen-file-at-point ()
-    (interactive)
-    (async-shell-command (concat "open " (dired-file-name-at-point)) "open (from dired)"))
-
-  (add-hook 'dired-mode-hook
-	    (=> (keymap-set dired-mode-map (kbd ";")
-			    'dired-sysopen-file-at-point))))
-
-(use-package dired-hide-dotfiles
-  :hook (dired-after-readin . dired-hide-dotfiles--hide)
-  :bind (:map dired-mode-map
-	      ("," . dired-hide-dotfiles-mode)))
-
-(setq dired-listing-switches "-alh")
-
-(defun xah-dired-sort ()
-  "Sort dired dir listing in different ways.
-Prompt for a choice.
-Works in linux, MacOS, Microsoft Windows.
-
-URL `http://xahlee.info/emacs/emacs/dired_sort.html'
-Created: 2018-12-23
-Version: 2025-01-05"
-  (interactive)
-  (let ((xmenu '(("date" . "-Al -t")
-                 ("size" . "-Al -S")
-                 ("name" . "-Al ")
-                 ("dir" . "-Al --group-directories-first")))
-        xsortBy)
-    (setq xsortBy (completing-read "Sort by (default date):"
-				   xmenu nil t nil nil (caar xmenu)))
-    (dired-sort-other (cdr (assoc xsortBy xmenu)))))
+  (keybind "C-x C-d" dired))
