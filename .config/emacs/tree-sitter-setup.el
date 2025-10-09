9983765400361dcad92584f6e7dadc7ec8b23cc2
Jake Freeland <jfree@FreeBSD.org>
2025-10-09 13:57:10 -0500

    emacs: Do not require tree-sitter
    use-package implicitly requires packages for us. No need to explicitly
    require them.

--- .config/emacs/tree-sitter-setup.el
+++ .config/emacs/tree-sitter-setup.el
@@ -4,12 +4,9 @@
 
 (use-package tree-sitter
   :config
-  (require 'tree-sitter)
   (global-tree-sitter-mode 1))
 
-(use-package tree-sitter-langs
-  :config
-  (require 'tree-sitter-langs))
+(use-package tree-sitter-langs)
 
 (add-hook
  'tree-sitter-after-on-hook #'tree-sitter-hl-mode)
