808191c85d3e3a19f5db32a580256dd817669783
Jake Freeland <jfree@FreeBSD.org>
2025-10-09 13:58:29 -0500

    emacs: Show code actions in margin
    Remove code actions from the eldoc echo area since they are
    distracting. Instead, keep them in the margin only.

--- .config/emacs/lsp-setup.el
+++ .config/emacs/lsp-setup.el
@@ -6,6 +6,7 @@
   :after orderless
   :init
   (setq eglot-autoshutdown t
+	eglot-code-action-indications '(margin)
 	completion-category-overrides '((eglot (styles orderless))
 					(eglot-capf (styles orderless))))
   :hook
