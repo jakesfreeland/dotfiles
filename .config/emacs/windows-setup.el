24427e7af2b359fed592d758bde446263051f94d
Jake Freeland <jfree@FreeBSD.org>
2025-10-10 15:20:20 -0500

    emacs: Update window management setup
    * Focus new windows on split.
    * Set new window switching keybinds.
    * Add new ace-window keybinds.

--- .config/emacs/windows-setup.el
+++ .config/emacs/windows-setup.el
@@ -1,14 +1,26 @@
 ;;;  -*- lexical-binding: t; -*-
-;;; buffers-setup.el -- Window management
+;;; windows-setup.el -- Window management
 ;;;
+
+(defun split-window-below-and-focus ()
+  (interactive)
+  (split-window-below)
+  (other-window 1))
+
+(defun split-window-right-and-focus ()
+  (interactive)
+  (split-window-right)
+  (other-window 1))
 
 (use-package emacs
   :config
-  ;; Easy window switch
-  (keybinds "C-<tab>" other-window
-            "S-C-i" enlarge-window
-            "S-C-d" shrink-window))
+  (keybinds "M-<tab>" other-window
+            "C-x 2" split-window-below-and-focus
+	    "C-x 3" split-window-right-and-focus
+	    "S-C-i" enlarge-window
+	    "S-C-d" shrink-window))
 
 (use-package ace-window
   :config
-  (keybind "C-z )" ace-swap-window))
+  (keybinds "C-z w" ace-window
+	    "C-z C-w" ace-swap-window))
