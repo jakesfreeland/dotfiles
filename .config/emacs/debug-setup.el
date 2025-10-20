a342f2fd2aabd6be4b7b92863f2ef867a39e200a
Jake Freeland <jfree@FreeBSD.org>
2025-10-20 10:47:00 -0500

    emacs: Fix up dape debugging configuration

--- .config/emacs/debug-setup.el
+++ .config/emacs/debug-setup.el
@@ -3,16 +3,10 @@
 ;;;
 
 (use-package dape
-  :preface
-  (setq dape-key-prefix (kbd "C-x C-a")
-	dape-debug t)
-
   :custom
-  (dape-breakpoint-global-mode +1)
-  (dape-buffer-window-arrangement 'gud)
-  ;; (dape-info-hide-mode-line nil)
+  (dape-breakpoint-global-mode 1)
+  (dape-buffer-window-arrangement 'right)
+  (dape-debug t)
 
   :config
-  (add-hook 'dape-display-source-hook #'pulse-momentary-highlight-one-line)
-  ;; Save buffers on startup, useful for interpreted languages
-  (add-hook 'dape-start-hook (lambda () (save-some-buffers t t))))
+  (add-hook 'dape-display-source-hook #'pulse-momentary-highlight-one-line))
